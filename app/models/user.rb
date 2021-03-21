381b50d61987725165d7c0ddfa3f6d312c13a069
horitomoyuki <eziltom0131@gmail.com>
2021-03-22 00:27:56 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :room_users
+  has_many :rooms, through: :room_users
   belongs_to :country
   has_one_attached :image
 
