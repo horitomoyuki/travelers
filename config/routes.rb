04f8ef8b1e06e092cfdfd385bf24d10baea9af25
horitomoyuki <eziltom0131@gmail.com>
2021-03-22 01:03:11 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :new, :edit, :update, :show]
 
   resources :rooms, only: [:new, :create, :destroy] do
-    resources :messages, only: [:index, :create]
+    resources :talks, only: [:index, :create]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_152219) do
+ActiveRecord::Schema.define(version: 2021_03_21_153538) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,16 @@ ActiveRecord::Schema.define(version: 2021_03_21_152219) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "talks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "room_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_talks_on_room_id"
+    t.index ["user_id"], name: "index_talks_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -67,4 +77,6 @@ ActiveRecord::Schema.define(version: 2021_03_21_152219) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
+  add_foreign_key "talks", "rooms"
+  add_foreign_key "talks", "users"
 end
