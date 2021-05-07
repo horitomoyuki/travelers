f2c0db139972c5228cd8b611d359fb3b13a53356
horitomoyuki <eziltom0131@gmail.com>
2021-05-07 21:38:29 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   
   devise_scope :user do
     post '/users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    # get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
   resources :users, only: [:index, :new, :edit, :update, :show] do
