11f9e9c22d4229c4c215c37636c295c3cb105b4b
horitomoyuki <eziltom0131@gmail.com>
2021-05-09 18:54:56 +0900

    修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,7 @@ class RelationshipsController < ApplicationController
   def create
     @follower = User.find(params[:follower_id])
     current_user.follow(@follower)
-    # render "create.js.erb"
+    render "create.js.erb"
   end
 
   def destroy
