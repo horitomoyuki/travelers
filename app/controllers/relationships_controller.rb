0cafb29169336a83e9223bcd1b9011a2d167a3cb
horitomoyuki <eziltom0131@gmail.com>
2021-05-10 15:16:32 +0900

    修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,5 +8,6 @@ class RelationshipsController < ApplicationController
   def destroy
     @follower = current_user.relationships.find_by(id: params[:id]).follower
     current_user.relationships.where(id: params[:id]).destroy_all
+    render "destroy.js.erb"
   end
 end
