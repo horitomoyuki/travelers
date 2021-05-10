13eadea281c2cfe593b8dbb7583292fa36dad10d
horitomoyuki <eziltom0131@gmail.com>
2021-05-11 06:08:47 +0900

    render layout: false

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
   def create
     @follower = User.find(params[:follower_id])
     current_user.follow(@follower)
-    render "create.js.erb"
+    render layout: false
   end
 
   def destroy
     @follower = current_user.relationships.find_by(id: params[:id]).follower
     current_user.relationships.where(id: params[:id]).destroy_all
-    render "destroy.js.erb"
+    render layout: false
   end
 end
