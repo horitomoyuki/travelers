5a3af5d801c8361c0cf69d0d8827fb78f00dd008
horitomoyuki <eziltom0131@gmail.com>
2021-05-11 13:35:59 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,11 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+require('jquery')
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
 document.addEventListener("turbolinks:load", () => {
   console.log($.fn.jquery)
 })
